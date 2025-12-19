3ed54597282a707e3654ce50d019f9e73b119063
Omar White <omar.white@cvshealth.com>
2025-12-19 14:40:59 -0500

    chore: adds vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ReactQueryProvider } from "../providers/react-query-provider";
 import Navbar from "./_components/Navbar";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -41,6 +42,7 @@ export default function RootLayout({
             <Navbar />
             {children}
             <Analytics />
+            <SpeedInsights />
           </ThemeProvider>
         </ReactQueryProvider>
       </body>
